20d0cda1ae84752f972dc90401b7e514c3bf9fa9
Seth Nordahl <sethnord812002@gmail.com>
2019-06-02 21:12:04 -0700

    End of day commit. 6/2/2019

--- P3DToForeFlight/Form1.Designer.cs
+++ P3DToForeFlight/Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(255, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 130);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect to FSUIPC";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label1.Location = new System.Drawing.Point(208, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FSUIPC CONNECTED!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 246);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "000.00 N";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "000.00 W";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(673, 356);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "P3DToForeFlight";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- P3DToForeFlight/Form1.cs
+++ P3DToForeFlight/Form1.cs
@@ -6,15 +6,96 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using FSUIPC;
+using System.Net.Sockets;
+using System.Net;
 
 namespace P3DToForeFlight
 {
     public partial class Form1 : Form
     {
+        //Global Vars
+        Offset groundSpeed = new Offset(692, 4);
+        FsPositionSnapshot playerPos = new FsPositionSnapshot();
+        UdpClient udp = new UdpClient(); //Create a UDP client, but DON'T bind it.
+        decimal lat;
+        decimal lon;
+        decimal alt;
+        decimal hdg;
+        UInt32 spd;
+
         public Form1()
         {
             InitializeComponent();
+            label1.Hide();
+            if (FSUIPCConnection.IsOpen)
+            {
+                label1.Show();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Connect to FSUIPC button
+            FSUIPCConnection.Open();
+
+            if (FSUIPCConnection.IsOpen)
+            {
+                label1.Show();
+                BroadcastPosition();
+            }
+        }
+
+        public void BroadcastPosition()
+        {
+            udp.Connect("192.168.1.103",49002);
+            string basePacket = "XGPSSimulator,";
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                
+                while (true)
+                {
+                    playerPos = FSUIPCConnection.GetPositionSnapshot();
+                    //Populate positional variables
+                    alt = (Decimal)playerPos.Altitude.Feet;
+                    FsLatLonPoint location = playerPos.Location;
+                    lat = (Decimal)location.Latitude.DecimalDegrees;
+                    lon = (Decimal)location.Longitude.DecimalDegrees;
+                    hdg = (Decimal)playerPos.HeadingDegreesTrue;
+                    FSUIPCConnection.Process();
+                    groundSpeed.Reconnect();
+                    spd = groundSpeed.GetValue<UInt32>();
+                    spd = spd / 65536;
+                    spd = spd * 3600;
+                    spd = spd / 1852;
+                    
+
+                    alt = Decimal.Round(alt, 1);
+                    lat = Decimal.Round(lat, 3);
+                    lon = Decimal.Round(lon, 3);
+                    hdg = Decimal.Round(hdg, 2);
+
+                    //Assemble a packet
+                    string packetToSend = basePacket + lon.ToString() + "," + lat.ToString() + "," + alt.ToString() + "," + hdg.ToString() + "," + spd.ToString() + ".0";
+                    byte[] sendMe = Encoding.ASCII.GetBytes(packetToSend);
+
+                    //Broadcast via UDP
+
+                    udp.Send(sendMe, sendMe.Length);
+
+                    //Pause the thread to allow for the one second gap required by foreflight.
+                    Thread.Sleep(990); //Only 990 to allow for processing time.
+                }
+
+            }).Start();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
